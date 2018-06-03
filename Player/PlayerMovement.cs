1cefc5c057a0c352b822d38243e9a8539b3e81c4
Dalton Pierce <dnnp2011@gmail.com>
2018-06-03 19:18:54 -0400

    First commit

---
+++ Player/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed = 6f;
+
+	private Vector3 movement;
+	private Animator anim;
+	private Rigidbody playerRigidbody;
+	private int floorMask;
+	private float camRayLength = 100f;
+
+	void Awake()
+	{
+		floorMask = LayerMask.GetMask ("Floor");
+		anim = GetComponent<Animator> ();
+		playerRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate()
+	{
+		// Does not return any values in between -1 and 1 (other than 0)
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		Move (h, v);
+
+		Turning ();
+
+		Animating (h, v);
+	}
+
+	void Move(float h, float v)
+	{
+		movement.Set (h, 0f, v);
+		movement = movement.normalized * speed * Time.deltaTime;
+
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+
+	void Turning()
+	{
+		// Cast a Ray from the camera to the mouse's pos on the Quad 'floor' to determine which way for the player to look
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		RaycastHit floorHit;
+
+		if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask))
+		{
+			Vector3 playerToMouse = floorHit.point - transform.position;
+			playerToMouse.y = 0f;
+
+			Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+			playerRigidbody.MoveRotation (newRotation);
+		}
+	}
+
+	void Animating(float h, float v)
+	{
+		bool walking = h != 0f || v != 0f;
+		anim.SetBool ("IsWalking", walking); // Set the bool param in the animator to this walking value
+	}
+
+}
